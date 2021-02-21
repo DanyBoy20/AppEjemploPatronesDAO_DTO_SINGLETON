1776bb6e06f54ee500bff552d9edecee9f79bcf5
Dany Hernandez <foxdan20@hotmail.com>
2021-02-21 14:15:37 -0600

    Comentario agregado

--- AppEjemploPatronesDAO_DTO_SINGLETON/DAO/ConexionBD.cs
+++ AppEjemploPatronesDAO_DTO_SINGLETON/DAO/ConexionBD.cs
@@ -10,6 +10,7 @@ namespace AppEjemploPatronesDAO_DTO_SINGLETON.DAO
 {
     class ConexionBD
     {
+        // CADENA CONEXION - AUTENTICACION S.O.
         protected SqlConnection Conexion = new SqlConnection("Server=DESKTOP-1NCENQ8;DataBase=Ejemplo_Patrones;Integrated Security=true");
     }
 }
